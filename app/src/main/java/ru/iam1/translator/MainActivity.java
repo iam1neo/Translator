83052788f547e164affbcd2d19db4cc38595e106
Ignatiev Anton <i.a.m.1@yandex.ru>
2017-04-22 13:11:55 +0300

    change langs

--- app/src/main/java/ru/iam1/translator/MainActivity.java
+++ app/src/main/java/ru/iam1/translator/MainActivity.java
@@ -5,40 +5,26 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TabHost;
 import android.widget.TextView;
+
+import java.util.Comparator;
+import java.util.HashMap;
 
 public class MainActivity extends Activity {
 
     private Translator translator;
-    private TextView mTextMessage;
 
     private void log(Object o){
         System.out.println(o);
     }
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_translate:
-                    translator.currentTab = item.getItemId();
-                    mTextMessage.setText(R.string.title_translate);
-                    return true;
-                case R.id.navigation_bookmarks:
-                    translator.currentTab = item.getItemId();
-                    mTextMessage.setText(R.string.title_bookmarks);
-                    return true;
-                case R.id.navigation_settings:
-                    translator.currentTab = item.getItemId();
-                    mTextMessage.setText(R.string.title_settings);
-                    return true;
-            }
-            return false;
-        }
-
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,11 +38,138 @@ public class MainActivity extends Activity {
             String langs = getIntent().getStringExtra(LoadingActivity.EXTRA_LANGS);
             translator = new Translator(langs);
         }
+        // инициализация вкладок
+        final TabHost tabHost = (TabHost) findViewById(R.id.tab_host);
+        tabHost.setup();
+        TabHost.TabSpec tabSpec;
+        // создаем главную вкладку переводчика
+        tabSpec = tabHost.newTabSpec(Translator.TAG_TRANSLATE);
+        tabSpec.setIndicator(getString(R.string.title_translate));
+        tabSpec.setContent(R.id.tab_translate);
+        tabHost.addTab(tabSpec);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-        navigation.setSelectedItemId(translator.currentTab);
+        //вкладка закладок
+        tabSpec = tabHost.newTabSpec(Translator.TAG_BOOKMARKS);
+        tabSpec.setIndicator(getString(R.string.title_bookmarks));
+        tabSpec.setContent(R.id.tab_bookmarks);
+        tabHost.addTab(tabSpec);
+
+        //вкладка настроек
+        tabSpec = tabHost.newTabSpec(Translator.TAG_SETTINGS);
+        tabSpec.setIndicator(getString(R.string.title_settings));
+        tabSpec.setContent(R.id.tab_settings);
+        tabHost.addTab(tabSpec);
+
+        //устанавливаем активную вкладку
+        tabHost.setCurrentTabByTag(translator.currentTabTag);
+
+        // обработчик переключения вкладок
+        tabHost.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+            public void onTabChanged(String tabTag) {
+                //запомним, какая вкладка теперь активная
+                translator.currentTabTag = tabTag;
+            }
+        });
+
+        // адаптер
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, translator.lang_names);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter.sort(new Comparator<String>() {
+            @Override
+            public int compare(String lhs, String rhs) {
+                return lhs.compareTo(rhs);
+            }
+        });
+
+        //Инициализация выпадающего списка Языка текста
+        final Spinner spn_lang_from = (Spinner) findViewById(R.id.spn_lang_from);
+        spn_lang_from.setAdapter(adapter);
+        spn_lang_from.setPrompt(getString(R.string.title_lang_from));
+        // выделяем выбранный язык
+        spn_lang_from.setSelection(translator.lang_from_index);
+        // без этого кода значение списка не обновится
+        spn_lang_from.post(new Runnable() {
+            @Override
+            public void run() {
+                spn_lang_from.setSelection(translator.lang_from_index);
+            }
+        });
+        // устанавливаем обработчик смены языка
+        spn_lang_from.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+                if(position == translator.lang_to_index){
+                    //если выбрали тот же язык, видимо хотели их поменять местами
+                    translator.lang_to_index = translator.lang_from_index;
+                    translator.lang_from_index = position;
+                    Spinner spn_lang_to = (Spinner) findViewById(R.id.spn_lang_to);
+                    spn_lang_to.setSelection(translator.lang_to_index);
+                    log("change langs");
+                }else{
+                    translator.lang_from_index = position;
+                }
+                log("translator.lang_to_index = "+translator.lang_to_index);
+                log("translator.lang_from_index = "+translator.lang_from_index);
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {}
+        });
+
+        //Инициализация выпадающего списка Языка перевода
+        final Spinner spn_lang_to = (Spinner) findViewById(R.id.spn_lang_to);
+        spn_lang_to.setAdapter(adapter);
+        spn_lang_to.setPrompt(getString(R.string.title_lang_to));
+        // выделяем выбранный язык
+        spn_lang_to.setSelection(translator.lang_to_index);
+        // без этого кода значение списка не обновится
+        spn_lang_to.post(new Runnable() {
+            @Override
+            public void run() {
+                spn_lang_to.setSelection(translator.lang_to_index);
+            }
+        });
+        // устанавливаем обработчик смены языка
+        spn_lang_to.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+                if(position == translator.lang_from_index){
+                    //если выбрали тот же язык, видимо хотели их поменять местами
+                    log("change langs");
+                    translator.lang_from_index = translator.lang_to_index;
+                    translator.lang_to_index = position;
+                    Spinner spn_lang_from = (Spinner) findViewById(R.id.spn_lang_from);
+                    spn_lang_from.setSelection(translator.lang_from_index);
+                }else{
+                    translator.lang_to_index = position;
+                }
+                log("translator.lang_to_index = "+translator.lang_to_index);
+                log("translator.lang_from_index = "+translator.lang_from_index);
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {}
+        });
+
+        //анимация смены вкладок
+        tabHost.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+            private int currentTab=tabHost.getCurrentTab();
+            public void onTabChanged(String tabId)
+            {
+                View currentView = tabHost.getCurrentView();
+                if (tabHost.getCurrentTab() > currentTab)
+                {
+                    currentView.setAnimation( inFromRightAnimation() );
+                    log("animation from right");
+                }
+                else
+                {
+                    currentView.setAnimation( outToRightAnimation() );
+                    log("animation to right");
+                }
+                currentTab = tabHost.getCurrentTab();
+            }
+        });
     }
 
     protected void onStart(){
@@ -89,10 +202,46 @@ public class MainActivity extends Activity {
         return translator;
     }
 
+    //обработка нажатий на кнопки
+    public void click(View v) {
+        switch (v.getId()){
+            case R.id.btn_change_langs:
+                //меняем первый язык на второй, второй сменится сам
+                Spinner spn_lang_from = (Spinner) findViewById(R.id.spn_lang_from);
+                spn_lang_from.setSelection(translator.lang_to_index);
+                break;
+        }
+    }
+
+    //попытка вернуться к предыдущему экрану = выход
     public void onBackPressed() {
         //super.onBackPressed();
         setResult(RESULT_OK, null);
         finish();
     }
 
+    //анимация смены вкладок
+    public Animation inFromRightAnimation()
+    {
+        Animation inFromRight = new TranslateAnimation(
+                Animation.RELATIVE_TO_PARENT, +1.0f,
+                Animation.RELATIVE_TO_PARENT, 0.0f,
+                Animation.RELATIVE_TO_PARENT, 0.0f,
+                Animation.RELATIVE_TO_PARENT, 0.0f);
+        inFromRight.setDuration(150);
+        inFromRight.setInterpolator(new AccelerateInterpolator());
+        return inFromRight;
+    }
+
+    public Animation outToRightAnimation()
+    {
+        Animation outtoLeft = new TranslateAnimation(
+                Animation.RELATIVE_TO_PARENT, -1.0f,
+                Animation.RELATIVE_TO_PARENT, 0.0f,
+                Animation.RELATIVE_TO_PARENT, 0.0f,
+                Animation.RELATIVE_TO_PARENT, 0.0f);
+        outtoLeft.setDuration(150);
+        outtoLeft.setInterpolator(new AccelerateInterpolator());
+        return outtoLeft;
+    }
 }
--- app/src/main/java/ru/iam1/translator/Translator.java
+++ app/src/main/java/ru/iam1/translator/Translator.java
@@ -1,5 +1,8 @@
 package ru.iam1.translator;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 
 /**
@@ -7,19 +10,57 @@ import java.util.HashMap;
  */
 
 public class Translator {
-    public int currentTab;//активная вкладка
-    public HashMap<String,String> langs;//коды языков и расшифровки
+    public static String TAG_TRANSLATE = "translate";
+    public static String TAG_BOOKMARKS = "bookmarks";
+    public static String TAG_SETTINGS = "settings";
+
+    public String[] lang_codes;//коды языков
+    public String[] lang_names;//расшифровки языков
+
+    public String currentTabTag;//активная вкладка
+    public int lang_from_index=-1;//индекс языка текста
+    public int lang_to_index=-1;//индекс языка перевода
 
     public Translator(String langs){
-        currentTab = R.id.navigation_translate;
-        //разберем строку с языками
-        this.langs = new HashMap<>();
+        //открывающаяся по умолчанию вкладка
+        currentTabTag = TAG_TRANSLATE;
+
+        //вспомогательные классы для сортировки языков по алфавиту
+        class Pair{
+            public String code;
+            public String name;
+            public Pair(String c, String n){
+                code=c;
+                name=n;
+            }
+        }
+        class PairComparator implements Comparator<Pair> {
+            @Override
+            public int compare(Pair pair1, Pair pair2) {
+                return pair1.name.compareTo(pair2.name);
+            }
+        }
+        //разбиваем строку с языками и сортируем
         String[] l1,l2;
         l1 = langs.split(";");
-        for (int i = 0; i < l1.length; i++){
+        int i;
+        ArrayList<Pair> langPairs = new ArrayList<>(l1.length);
+        for (i = 0; i < l1.length; i++){
             l2 = l1[i].split("=");
-            this.langs.put(l2[0],l2[1]);
+            langPairs.add(new Pair(l2[0],l2[1]));
+        }
+        Collections.sort(langPairs,new PairComparator());
+
+        //заполним массивы кодов и расшифровок поддерживаемых языков
+        lang_codes = new String[l1.length];
+        lang_names = new String[l1.length];
+        for(i=0; i<l1.length; i++){
+            lang_codes[i] = langPairs.get(i).code;
+            lang_names[i] = langPairs.get(i).name;
+            if(lang_codes[i].equals("ru"))
+                lang_from_index = i;
+            if(lang_codes[i].equals("en"))
+                lang_to_index = i;
         }
     }
-
 }
